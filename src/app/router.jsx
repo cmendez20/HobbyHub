efb6343dd87924426cbf51813e8d5f7f6c83a7ae
Chris Mendez <mendecj812@gmail.com>
2024-11-06 14:43:37 -0600

    feat: setup react router

---
+++ src/app/router.jsx
@@ -0,0 +1,26 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: 'about',
+    element: <div>About</div>,
+  },
+]);
+
+export function Router() {
+  return <RouterProvider router={router} />;
+}
