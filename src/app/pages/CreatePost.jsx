e9d0dcb7f9cdd98afebdb17519e8e0d9d66eb8ff
Chris Mendez <mendecj812@gmail.com>
2024-11-07 11:08:33 -0600

    feat: setup static version of create post form

--- src/app/pages/CreatePost.jsx
+++ src/app/pages/CreatePost.jsx
@@ -1,7 +1,34 @@
+import { useForm } from 'react-hook-form';
+
 const CreatePost = () => {
   return (
-    <div>
-      <p>Creat post</p>
+    <div className="max-w-2xl mx-auto pt-16">
+      <form className="grid gap-8">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          className="px-4 py-2 rounded-lg"
+        />
+        <textarea
+          name="content"
+          id="content"
+          placeholder="Content (Optional)"
+          className="px-4 py-2 rounded-lg min-h-48"
+        ></textarea>
+        <input
+          type="text"
+          name="imageURL"
+          placeholder="Image URL (Optional)"
+          className="px-4 py-2 rounded-lg"
+        />
+        <button
+          type="submit"
+          className="px-5 py-2.5 bg-slate-600 text-white justify-self-start rounded-lg transition-colors hover:bg-slate-800"
+        >
+          Create post
+        </button>
+      </form>
     </div>
   );
 };
