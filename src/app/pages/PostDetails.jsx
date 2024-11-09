548eca6d4408fccc68cedd315290b9207a601a23
chris mendez mba <mendecj812@gmail.com>
2024-11-08 21:35:39 -0600

    build: add tanstack query package

--- src/app/pages/PostDetails.jsx
+++ src/app/pages/PostDetails.jsx
@@ -1,26 +1,46 @@
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { formatDistanceToNow, parseISO } from 'date-fns';
+import { useParams, Link } from "react-router-dom";
+import { formatDistanceToNow, parseISO } from "date-fns";
+import { supabase } from "../supabase-client";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchPost = async (id) => {
+  const { data, error } = await supabase
+    .from("posts")
+    .select()
+    .eq("id", id)
+    .single();
+
+  if (error) throw error;
+  return data;
+};
 
 const PostDetails = () => {
   const { id } = useParams();
+  const {
+    data: post,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["post", id],
+    queryFn: () => fetchPost(id),
+  });
 
-  // const postedDate = formatDistanceToNow(parseISO(created_at), {
-  //   addSuffix: true,
-  // });
-  // console.log(id);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error {error.message}</div>;
 
-  // get post
+  const postedDate = formatDistanceToNow(parseISO(post.created_at), {
+    addSuffix: true,
+  });
 
   return (
     <section className="max-w-5xl mx-auto pt-8">
       <div className="bg-white p-8 rounded-lg flex flex-col gap-4 mb-8">
-        <p className="font-extralight">timestamp</p>
-        <p className="font-bold text-xl">post title</p>
-        <p className="font-extralight">Post Content</p>
-        <p className="font-extralight">Post Image</p>
+        <p className="font-extralight">{postedDate}</p>
+        <p className="font-bold text-xl">{post.title}</p>
+        <p className="font-extralight">{post.content}</p>
+        {post.image_url && <img src={post.image_url} alt="Post image" />}
         <div className="flex justify-between items-center">
-          <p className="font-extralight"># of upvotes</p>
+          <p className="font-extralight">{post.upvotes} upvotes</p>
           <div className="flex gap-4">
             <Link
               to={`edit`}
@@ -35,8 +55,9 @@ const PostDetails = () => {
         </div>
       </div>
       <div className="bg-gray-100 p-4 rounded-lg grid gap-4">
-        <p>- Did you forget about Ben Franklin?</p>
-        <p>- It's got to be Geourge Washington!</p>
+        {post.comments.map((comment, i) => (
+          <p key={i}>- {comment}</p>
+        ))}
         <input type="text" placeholder="Leave a comment..." className="p-2" />
       </div>
     </section>
--- src/app/pages/RootLayout.jsx
+++ src/app/pages/RootLayout.jsx
@@ -1,15 +1,26 @@
-import { Navbar } from '../../components/Navbar';
-import { Outlet } from 'react-router-dom';
-import { SearchProvider } from '../SearchContext';
+import { Navbar } from "../../components/Navbar";
+import { Outlet } from "react-router-dom";
+import { SearchProvider } from "../SearchContext";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const RootLayout = () => {
   return (
-    <SearchProvider>
-      <Navbar />
-      <div className="bg-gray-50 min-h-dvh">
-        <Outlet />
-      </div>
-    </SearchProvider>
+    <QueryClientProvider client={queryClient}>
+      <SearchProvider>
+        <Navbar />
+        <div className="bg-gray-50 min-h-dvh">
+          <Outlet />
+        </div>
+      </SearchProvider>
+    </QueryClientProvider>
   );
 };
 
